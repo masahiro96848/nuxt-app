6af22863f315800b6ac1a80ee7297bd92bc7e336
masahiro96848 <feetwcpic9436@gmail.com>
2022-10-31 09:09:55 +0900

    feat: レイアウトを追加。ライフサイクルを使用

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,21 @@
     index<br />
     <NuxtLink to="/about">about</NuxtLink>
     <NuxtLogo />
-    <Header />
+    <BaseHeader />
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  asyncData() {
+    console.log('asyncData')
+  },
+  created() {
+    console.log('created')
+  },
+  fetch(context) {
+    console.log('fetch')
+    console.log(context.route)
+  },
+}
 </script>
